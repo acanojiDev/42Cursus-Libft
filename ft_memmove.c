11781a93bb4a80c0efe44bb5acd61b3585547dba
Antonio Lorenzo Cano Jimenez <acano-ji@c3r7s3.42malaga.com>
2024-10-18 17:49:37 +0200

    memmove y strnstr completadas

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: acano-ji <acano-ji@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 15:14:21 by acano-ji          #+#    #+#             */
-/*   Updated: 2024/09/23 14:42:50 by acano-ji         ###   ########.fr       */
+/*   Updated: 2024/10/18 17:13:10 by acano-ji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,16 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
-	size_t				i;
-	unsigned char		temp [10];
 
-	d = (unsigned char *) dest;
-	s = (const unsigned char *) src;
-	i = 0;
-	
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d > s)
+		while (n--)
+			d[n] = s[n];
+	else
+		while (n--)
+			*d++ = *s++;
+	return (dest);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,14 +6,33 @@
 /*   By: acano-ji <acano-ji@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 15:37:19 by acano-ji          #+#    #+#             */
-/*   Updated: 2024/09/23 15:04:00 by acano-ji         ###   ########.fr       */
+/*   Updated: 2024/10/18 17:48:07 by acano-ji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	if (*little == '\0')
-		return ((char *)big);
+	const char	*n;
+
+	n = (const char *)needle;
+	if (!*needle)
+		return (haystack);
+	while (*haystack && len--)
+	{
+		if (*haystack == *needle)
+		{
+			while (*n && len > 0 && *haystack == *n)
+			{
+				haystack++;
+				n++;
+				len--;
+			}
+			if (*n == '\0')
+				return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
 }
