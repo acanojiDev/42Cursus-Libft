0a36da42c8c1f0afc2e5d6e40690dc9032584114
Antonio Lorenzo Cano Jimenez <acano-ji@c3r10s2.42malaga.com>
2024-11-15 15:14:15 +0100

    parte obligatoria terminada y comienzo de bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acano-ji <acano-ji@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 14:46:07 by acano-ji          #+#    #+#             */
+/*   Updated: 2024/11/15 14:57:13 by acano-ji         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if(!new)
+		return (NULL);
+	new ->content = content;
+	new -> next = NULL;
+	return (new);
+}
--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,56 @@
 /*   By: acano-ji <acano-ji@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 18:18:02 by acano-ji          #+#    #+#             */
-/*   Updated: 2024/11/10 18:18:07 by acano-ji         ###   ########.fr       */
+/*   Updated: 2024/11/15 14:49:48 by acano-ji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static size_t	ft_countwords(char const *s, char c)
+{
+	size_t	count;
+
+	if (!*s)
+		return (0);
+	count = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			count++;
+		while (*s != c && *s)
+			s++;
+	}
+	return (count);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**lst;
+	size_t	word_len;
+	int		i;
+
+	lst = (char **)malloc((ft_countwords(s, c) + 1) * sizeof(char *));
+	if (!s || !lst)
+		return (0);
+	i = 0;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s)
+		{
+			if (!ft_strchr(s, c))
+				word_len = ft_strlen(s);
+			else
+				word_len = ft_strchr(s, c) - s;
+			lst[i++] = ft_substr(s, 0, word_len);
+			s += word_len;
+		}
+	}
+	lst[i] = NULL;
+	return (lst);
 }
+/*el free al main, liberar si falla substr.*/
